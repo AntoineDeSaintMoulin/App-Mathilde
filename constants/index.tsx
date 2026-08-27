d1b2a351bf46d5c8d22fd1ddfe2935b0aa1befc9
AntoineDeSaintMoulin <91260719+AntoineDeSaintMoulin@users.noreply.github.com>
2026-08-27 13:45:38 +0200

    Update index.tsx

--- constants/index.tsx
+++ constants/index.tsx
@@ -14,3 +14,10 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+export const getCycleFromDate = (dateStr: string): 1 | 2 | 3 => {
+  const month = new Date(dateStr).getMonth() + 1;
+  if (month >= 8 && month <= 11) return 1;
+  if (month === 12 || (month >= 1 && month <= 3)) return 2;
+  return 3;
+};
