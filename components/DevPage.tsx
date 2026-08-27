9de60544f3a78fb6e30779df91bcc0f306471d45
AntoineDeSaintMoulin <91260719+AntoineDeSaintMoulin@users.noreply.github.com>
2026-08-27 12:19:59 +0200

    Add inModal prop to DevPage component

--- components/DevPage.tsx
+++ components/DevPage.tsx
@@ -25,7 +25,7 @@ const SUBJECTS = [
 const PRIMARY_YEARS = [1, 2, 3, 4, 5, 6];
 const SECONDARY_YEARS = [1, 2, 3, 4, 5, 6];
 
-const DevPage: React.FC<Props> = ({ userId, fullName, subjects, years, onProfileUpdated }) => {
+const DevPage: React.FC<Props> = ({ userId, fullName, subjects, years, onProfileUpdated, inModal = false }) => {
   const [showEdit, setShowEdit] = useState(false);
   const [selectedSubjects, setSelectedSubjects] = useState<string[]>(subjects);
   const [selectedPrimaryYears, setSelectedPrimaryYears] = useState<number[]>(
