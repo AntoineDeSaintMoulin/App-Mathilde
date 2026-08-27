2d104517c11f22f41412f9a66bef32696ab7f789
AntoineDeSaintMoulin <91260719+AntoineDeSaintMoulin@users.noreply.github.com>
2026-08-27 10:36:57 +0200

    Update AuthScreen.tsx

--- components/AuthScreen.tsx
+++ components/AuthScreen.tsx
@@ -12,7 +12,7 @@ const AuthScreen: React.FC = () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.location.origin,
+        redirectTo: 'https://app-mathilde.vercel.app',
       },
     });
     if (error) {
